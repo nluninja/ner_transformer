c0ab5d22883a862fc9e2468ffac0ee8f57d700e4
pepistrafforello <pepi.strafforello@gmail.com>
2021-06-23 16:26:52 +0000

    # of checkpoints reduced to save disk.

--- run_wiki-org.sh
+++ run_wiki-org.sh
@@ -1,1 +1,10 @@
-python3 run_ner.py --model_type bert --model_name_or_path bert-base-cased --data_dir data/wiki-org --labels data/wiki-org/labels.txt --output_dir output.wiki-org --overwrite_cache --overwrite_output_dir --do_train
+python3 run_ner.py \
+	--model_type bert \
+	--model_name_or_path bert-base-cased \
+	--data_dir data/wiki-org \
+	--labels data/wiki-org/labels.txt \
+	--output_dir output.wiki-org \
+	--save_steps 1000 \
+	--overwrite_cache \
+	--overwrite_output_dir \
+	--do_train
