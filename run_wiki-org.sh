5baaf1b9289cee741a6deef96542a1863b2f0024
pepistrafforello <pepi.strafforello@gmail.com>
2021-06-23 15:49:57 +0000

    first successful training with wiki-org, results to be evaluated.

---
+++ run_wiki-org.sh
@@ -0,0 +1,1 @@
+python3 run_ner.py --model_type bert --model_name_or_path bert-base-cased --data_dir data/wiki-org --labels data/wiki-org/labels.txt --output_dir output.wiki-org --overwrite_cache --overwrite_output_dir --do_train
