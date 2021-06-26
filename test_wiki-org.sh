4259301858703051f09332a99a6f3cbf1176bdac
pepistrafforello <pepi.strafforello@gmail.com>
2021-06-26 12:51:23 +0000

    more launch scripts added.

--- test_wiki-org.sh
+++ test_wiki-org.sh
@@ -1,7 +1,7 @@
 python3 run_ner.py \
 	--model_type bert \
-	--model_name_or_path bert-base-cased \
+	--model_name_or_path bert-large-cased-whole-word-masking \
 	--data_dir data/wiki-org \
 	--labels data/wiki-org/labels.txt \
-	--output_dir output.wiki-org \
+	--output_dir output.wiki-org.bert-large-cased-whole-word-masking \
 	--do_predict
